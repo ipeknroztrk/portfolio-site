03e3879c3b993886ad11a39d3ab4852dae0c0e02
ipek nur <114228895+ipeknroztrk@users.noreply.github.com>
2026-05-13 00:02:05 +0300

    improve ping endpoint with db check

--- PortfolioSite.API/Controllers/PingController.cs
+++ PortfolioSite.API/Controllers/PingController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PortfolioSite.Application.Data;
 
 namespace PortfolioSite.API.Controllers
 {
@@ -6,10 +8,23 @@ namespace PortfolioSite.API.Controllers
     [Route("api/ping")]
     public class PingController : ControllerBase
     {
+        private readonly AppDbContext _context;
+
+        public PingController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Ping()
         {
-            return Ok("pong");
+            var canConnect = _context.Database.CanConnect();
+
+            return Ok(new
+            {
+                status = "pong",
+                database = canConnect
+            });
         }
     }
 }
